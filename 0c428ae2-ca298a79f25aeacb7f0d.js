75d016251affb6f0d80621a2d60a6fd8f9f1c3c7
Rajeshwar Kashyap <work.rjkashyap05@gmail.com>
2022-09-04 20:21:26 +0530

    Updates

---
+++ 0c428ae2-ca298a79f25aeacb7f0d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_portfolio=self.webpackChunkmy_portfolio||[]).push([[13],{1852:function(t,o,r){r.d(o,{xTs:function(){return n}});var e=r(1046);function n(t){return(0,e.w_)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"}}]})(t)}}}]);
+//# sourceMappingURL=0c428ae2-ca298a79f25aeacb7f0d.js.map
--- gatsby-browser.js
+++
@@ -1,2 +0,0 @@
-import './src/styles/global.scss';
-import './src/styles/custom.css';
